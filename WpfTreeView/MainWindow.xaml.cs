26379637bebf2bbef7dc95dc0a91a827281f6bd8
Davoleo <davoleo2001@gmail.com>
2020-02-15 18:59:09 +0100

    Recursive Logic to Scan Folder and Files

--- WpfTreeView/MainWindow.xaml.cs
+++ WpfTreeView/MainWindow.xaml.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTreeView
 {
@@ -42,17 +33,161 @@ namespace WpfTreeView
         /// <param name="e"></param>
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
+            //Get all the logical drives
             foreach (var drive in Directory.GetLogicalDrives())
             {
-                var item = new TreeViewItem();
+                //Create a new TreeViewItem for each Drive
+                var item = new TreeViewItem()
+                {
+                    //Set the Header and the full path path of the TVITem
+                    Header = drive,
+                    Tag = drive
+                };
 
-                item.Header = drive;
-                item.Tag = drive;
+                //Dummy Item
+                item.Items.Add(null);
 
+                //Listener to the OnExpand Event
+                item.Expanded += Folder_Expanded;
+
+                //Add Item to the TreeView
                 FolderView.Items.Add(item);
             }
         }
 
+        #region Folder Expanded Event
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+
+            var expandedItem = (TreeViewItem) sender;
+
+            //If there's no dummy item
+            if (expandedItem.Items.Count != 1 || expandedItem.Items[0] != null)
+                return;
+
+            //Remove the dummy
+            expandedItem.Items.Clear();
+
+            //Full Path
+            var path = (string) expandedItem.Tag;
+
+            #region Get Folders
+
+            //Blank list
+            var directories = new List<string>();
+
+            //Try to gather directories from the folder
+            try
+            {
+                var dirs = Directory.GetDirectories(path);
+
+                if (dirs.Length > 0)
+                {
+                    directories.AddRange(dirs);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            //for each Directory...
+            directories.ForEach(dirPath =>
+            {
+                //Create the sub-dir Item
+                var subItem = new TreeViewItem()
+                {
+                    //set its folder name
+                    Header = GetFileFolderName(dirPath),
+
+                    Tag = dirPath
+                };
+
+                //Add a dummy to allow folder expansion
+                subItem.Items.Add(null);
+
+                //Handle further expansion
+                subItem.Expanded += Folder_Expanded;
+
+                //Add Sub item to the parent
+                expandedItem.Items.Add(subItem);
+            });
+
+            #endregion
+
+            #region Get Files
+
+            //Blank list
+            var files = new List<string>();
+
+            //Try to gather files from the folder
+            try
+            {
+                var f = Directory.GetFiles(path);
+
+                if (f.Length > 0)
+                {
+                    files.AddRange(f);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+
+            //for each File...
+            files.ForEach(filePath =>
+            {
+                //Create the sub-dir Item
+                var subItem = new TreeViewItem()
+                {
+                    //set its file name
+                    Header = GetFileFolderName(filePath),
+
+                    Tag = filePath
+                };
+
+                //Add Sub item to the parent
+                expandedItem.Items.Add(subItem);
+            });
+
+
+
+            #endregion
+        }
+
         #endregion
+
+
+        /// <summary>
+        /// Find the file or the folder name from a full path
+        /// </summary>
+        /// <param name="path">the full path</param>
+        /// <returns>everything after the last slash</returns>
+        public static string GetFileFolderName(string path)
+        {
+            // "C:\Everything\Images" --> "Images"
+
+            //return an empoty string if there's no path
+            if (string.IsNullOrEmpty(path))
+                return String.Empty;
+
+            //Make sure all the slashes are backwards
+            var normalizedPath = path.Replace('/', '\\');
+
+            //Find the last backslash
+            var lastEntryIndex = normalizedPath.LastIndexOf('\\');
+
+            if (lastEntryIndex <= 0)
+                return path;
+            
+            //Return everything after the last slash
+            return path.Substring(lastEntryIndex + 1);
+        }
+
+        #endregion
+
+
     }
 }
