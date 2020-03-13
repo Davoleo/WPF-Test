20c84dbeac27f190f905164ca4d42563583e8b6d
Davoleo <davoleo2001@gmail.com>
2020-03-13 18:01:47 +0100

    View Models (UI Logic)

--- WpfTreeView/Directory/ViewModels/DirectoryItemViewModel.cs
+++ WpfTreeView/Directory/ViewModels/DirectoryItemViewModel.cs
@@ -1,9 +1,16 @@
-﻿using WpfTreeView.Directory.Data;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using WpfTreeView.Directory.Data;
 
 namespace WpfTreeView.Directory.ViewModels
 {
+    // More UI-Specific than the internal object
     class DirectoryItemViewModel
     {
+
+        #region Public Properties
+
         /// <summary>
         /// The full path of the Directory Item
         /// </summary>
@@ -18,5 +25,94 @@ namespace WpfTreeView.Directory.ViewModels
         /// The name of this directory Item
         /// </summary>
         public string Name => Type == DirectoryItemType.Drive ? FullPath : DirectoryStructure.GetFileFolderName(FullPath);
+
+        /// <summary>
+        /// The list of all the Children of this Directory Item
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Children { get; set; }
+
+        /// <summary>
+        /// indicates whether this item can be expanded
+        /// </summary>
+        public bool CanExpand => Type != DirectoryItemType.File;
+
+        /// <summary>
+        /// Tells if the current item is expanded or not
+        /// </summary>
+        public bool IsExpanded
+        {
+            get { return Children.Count(item => item != null) > 0; }
+            set
+            {
+                //if the UI tells us to expand
+                if (value)
+                    Expand();
+                else
+                    ClearChildren();
+            }
+        }
+
+        #endregion
+
+        #region Public Commands
+
+        /// <summary>
+        /// The command to expand this item
+        /// </summary>
+        public ICommand ExpandCommand { get; set; }
+
+        #endregion
+
+
+        /// <summary>
+        /// Default Constructor
+        /// <param name="fullPath">The full path of this item</param>
+        /// <param name="type">The Type of this item</param>
+        /// </summary>
+        public DirectoryItemViewModel(string fullPath, DirectoryItemType type)
+        {
+            //create the commands
+            ExpandCommand = new RelayCommand(Expand);
+
+            //set the properties
+            FullPath = fullPath;
+            Type = type;
+
+            //Setup the children as needed
+            ClearChildren();
+        }
+
+        /// <summary>
+        /// Expands this item and finds all the children
+        /// </summary>
+        private void Expand()
+        {
+            if (Type == DirectoryItemType.File)
+                return;
+
+            //Find All the Children
+            Children = new ObservableCollection<DirectoryItemViewModel>(
+                DirectoryStructure.GetDirectoryContents(this.FullPath)
+                    .Select(content => new DirectoryItemViewModel(content.FullPath, content.Type))
+            );
+        }
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Removes all children from the Observable Collection
+        /// Adding a dummy item to show the arrow icon if necessary
+        /// </summary>
+        private void ClearChildren()
+        {
+            //Clears all the items of the old list by assigning a new empty list
+            Children = new ObservableCollection<DirectoryItemViewModel>();
+
+            //Show the expand arrow if type != File
+            if (Type != DirectoryItemType.File )
+                Children.Add(null);
+        }
+
+        #endregion
     }
 }
---
+++ WpfTreeView/Directory/ViewModels/DirectoryStructureViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace WpfTreeView.Directory.ViewModels
+{
+
+    /// <summary>
+    /// View Model for the application main tree view
+    /// </summary>
+    class DirectoryStructureViewModel : BaseViewModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// A list of all directories on the machine
+        /// </summary>
+        public ObservableCollection<DirectoryItemViewModel> Items { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public DirectoryStructureViewModel()
+        {
+            //Get the logical Drives
+            var children = DirectoryStructure.GetLogicalDrives();
+
+            //Create view models from the data
+            Items = new ObservableCollection<DirectoryItemViewModel>(
+                children.Select(drive => new DirectoryItemViewModel(drive.FullPath, drive.Type))
+            );
+        }
+
+    }
+}
