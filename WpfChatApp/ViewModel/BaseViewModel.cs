c659d49df2d38c9a4318f7f8e30d64007f465ff8
Davoleo <davoleo2001@gmail.com>
2020-04-14 13:00:53 +0200

    chat app first commit

---
+++ WpfChatApp/ViewModel/BaseViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace WpfChatApp.ViewModel
+{
+    /// <summary>
+    /// A Base View Model that fires Property Changed Events
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The event that is fired when properties change value
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => {};
+
+        /// <summary>
+        /// Call this to fire <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ WpfChatApp/ViewModel/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfChatApp.ViewModel
+{
+
+    /// <summary>
+    /// A General Command that runs an Action
+    /// </summary>
+    class RelayCommand : ICommand
+    {
+
+        #region Private Members
+
+        private Action action;
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// The fired event when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => {};
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public RelayCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        #endregion
+
+        #region Command Methods
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        #endregion
+    }
+}
